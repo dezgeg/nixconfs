be8e476e8250825e1c9a6a1b604451c9bd876b94
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-07-23 15:49:31 +0300

    raspi.nix: Init

---
+++ raspi.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }: {
+  imports = [
+      ./arm-board.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  networking.hostName = "raspi";
+  networking.hostId = "8f076ab4";
+
+  boot.kernelParams = ["smsc95xx.macaddr=b8:27:eb:63:84:ca"];
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_rpi;
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXOS_BOOT";
+      fsType = "vfat";
+    };
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+}
