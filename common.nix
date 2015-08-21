04e5fa0a8608e6dd548d9ad70180d6e6d9fdf52f
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-08-21 22:24:08 +0300

    common.nix: Use nix.trustedUsers
    Now we can copy-closure between machines without root

--- common.nix
+++ common.nix
@@ -88,6 +88,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   nixpkgs.config.allowUnfree = true;
+  nix.trustedUsers = ["tmtynkky"];
   nix.binaryCachePublicKeys = [
     "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
     "nixos-arm.dezgeg.me-1:xBaUKS3n17BZPKeyxL4JfbTqECsT+ysbDJz29kLFRW0=%"
